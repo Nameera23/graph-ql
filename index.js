21b67c08e39dd6f2d9b42f994410c0aec2b1881d
Nameera khan <nameerakhan190@gmail.com>
2023-05-12 12:24:01 +0500

    CRUD api

---
+++ index.js
@@ -0,0 +1,18 @@
+const {ApolloServer} = require('apollo-server');
+const mongoose= require('mongoose')
+
+const MONGODB='mongodb+srv://nameera:<password>@cluster0.arfwhew.mongodb.net/?retryWrites=true&w=majority'
+
+const typeDefs=require('./garphql/typeDefs')
+const resolvers=require('./garphql/resolvers')
+const server= new ApolloServer({
+    typeDefs,resolvers
+})
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+.then(()=>{
+    console.log("MongoDB connect successful")
+    return server.listen({port: 4000})
+})
+.then((res)=>{
+    console.log(`server running on port ${res.url}`)
+})
